67843c461c2d1effbefebabfc27463bd5659e84a
Ma Lian <malhch@gmail.com>
2014-01-05 23:50:50 +0800

    2014-01-05 23:50 Config: make port and livereload configurable from command line

--- Gruntfile.js
+++ Gruntfile.js
@@ -42,8 +42,8 @@ module.exports = function (grunt) {
         },
         connect: {
             options: {
-                port: 9000,
-                livereload: 35729,
+                port: grunt.option('port') || 9000,
+                livereload: 35729 + (grunt.option('livereload') || 0),
                 // change this to '0.0.0.0' to access the server from outside
                 hostname: 'localhost'
             },
