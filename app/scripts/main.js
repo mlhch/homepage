e3fb2e2b8c867be9bf7e30b39e040f8adf13cfe8
Ma Lian <malhch@gmail.com>
2014-01-22 17:07:01 +0800

    2014-01-22 17:04 Use: fancybox(depend require-css)

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,9 +1,18 @@
 require({
     waitSeconds: 0,
+    map: {
+        '*': {
+            'css': 'require-css/css.min'
+        }
+    },
     paths: {
         d3: 'd3/d3'
     },
     shim: {
+        fancybox: {
+            deps: ['fancybox/source/jquery.fancybox.pack', 'css!fancybox/source/jquery.fancybox'],
+            exports: '$.fancybox'
+        },
         'd3': {
             exports: 'd3'
         }
